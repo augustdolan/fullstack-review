1b8571dfc5e4f1d3d4dedf695da0bad9d99e01d5
August Dolan <august.dolan1@gmail.com>
2020-09-15 12:57:53 -0700

    Fix client bug and update to query server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,18 +4,27 @@ import $ from 'jquery';
 import Search from './components/Search.jsx';
 import RepoList from './components/RepoList.jsx';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       repos: []
     }
 
   }
 
   search (term) {
-    console.log(`${term} was searched`);
-    // TODO
+  console.log(`${term} was searched`);
+  $.ajax({
+    type: "POST",
+    url: '/repos',
+    data: JSON.stringify({username: term}),
+    success: function(err, result) {
+      console.log(err, result);
+    },
+    contentType: 'application/json'
+  });
   }
 
   render () {
